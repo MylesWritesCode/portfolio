04e0b130502376c619d6da0a0ecc45dde68cc7d7
Myles Berueda <myles.berueda@codemithril.com>
2021-11-20 20:45:18 -1000

    added chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/**
+ * File: /src/pages/_app.tsx
+ * Project: portfolio
+ * Purpose: Entry to the landing page
+ *
+ * @author Myles Berueda
+ * @date   Saturday November 20th 2021
+ * -----
+ * Modified: Saturday November 20th 2021 4:55:46 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ **/
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
