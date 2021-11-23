fc9ac5fb87c8f546c3a5b488b6714e835b5cda10
Myles Berueda <myles.berueda@codemithril.com>
2021-11-22 16:41:37 -1000

    messing with error messages. wont go to prod

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -22,6 +22,12 @@ const Welcome: React.FC<WelcomeProps> = ({ ...props }) => {
   const titleLine: string = "Hi! I'm Myles :)";
   const skills: string[] = ["web"];
 
+  console.log(
+    "%c\n%chmmmmmm 🤔",
+    "",
+    "border: 2px solid #9e9595; padding: 0.5em; background-color: #2f2f2f; font-size: 50px; text-transform: uppercase; color: red; text-shadow: -2px -2px red, 4px 4px orange, 4px 4px yellow, -6px -6px green, 10px 10px blue, -12px -12px purple;"
+  )
+
   const typewritter = (sentences: string[]) => {};
 
   return (
