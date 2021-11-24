2c4ae7a9ef884ef26595d58e06496f0e6726148a
Myles Berueda <myles.berueda@codemithril.com>
2021-11-24 00:29:28 -1000

    added some content to about me section
    Still don't know what picture I'm going to use. I have, like, no good
    pictures of me. Maybe Mel has one idk.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,7 +2,7 @@
  * File: /src/components/AboutMe.tsx
  * Project: portfolio
  * Purpose: // TODO: Add the purpose of the file here.
- * 
+ *
  * @author Myles Berueda
  * @date   Tuesday November 23rd 2021
  * -----
@@ -11,9 +11,9 @@
  * Copyright (c) 2021 MylesWritesCode
  * -----
  * HISTORY
-**/
-import React from 'react';
-import { chakra } from '@chakra-ui/react';
+ **/
+import React from "react";
+import { chakra } from "@chakra-ui/react";
 
 interface AboutMeProps {}
 
@@ -24,7 +24,7 @@ const AboutMe: React.FC<AboutMeProps> = ({ ...props }) => {
       data-scroll="about-me"
       display="flex"
       height="80vh"
-      justifyContent="center"
+      justifyContent="space-evenly"
       alignItems="center"
       flexDirection={["column", "row"]}
       backgroundSize="cover"
@@ -33,12 +33,20 @@ const AboutMe: React.FC<AboutMeProps> = ({ ...props }) => {
       color="#FFFFFF"
     >
       <chakra.div>
-        <chakra.h2>
-          About me
-        </chakra.h2>
+        <chakra.h1 fontFamily="Lato" fontSize="3rem" fontWeight="900">about me</chakra.h1>
+        <chakra.p>This should be my whole about me thingy</chakra.p>
       </chakra.div>
+      <chakra.div
+        backgroundImage="guitar-dude.jpg"
+        backgroundPosition="center"
+        backgroundSize="cover"
+        width="100%"
+        boxSize={["300px"]}
+        border="3px solid #FFFFFF"
+        borderRadius="50%"
+      />
     </chakra.div>
   );
-}
+};
 
 export default AboutMe;
