b5d9f4b806d6abaae0a8a5938126460124a82b2d
MylesWritesCode <myles@themapletree.io>
2022-06-29 00:35:37 -1000

    fixes deploy

--- src/components/ImageWithModal.tsx
+++ src/components/ImageWithModal.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from "react";
 
-/* eslint-disable @next/next/no-img-element */
 interface ImageWithModalProps {
   href: string;
   className?: string;
